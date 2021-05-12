47cb045a1f1969db36bcc7913d23f61a094b471a
Javel Wilson <javelawilson@gmail.com>
2021-05-12 00:05:39 -0500

    Define user schema middleware to salt and hash the password

--- src/models/User.js
+++ src/models/User.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose')
+const bcrypt = require('bcryptjs')
 
 const userSchema = new mongoose.Schema({
   email: {
@@ -10,6 +11,11 @@ const userSchema = new mongoose.Schema({
     type: String,
     required: true,
   },
+})
+
+userSchema.pre('save', async function (next) {
+  const salt = await bcrypt.genSalt(10)
+  this.password = await bcrypt.hash(this.password, salt)
 })
 
 mongoose.model('User', userSchema)
